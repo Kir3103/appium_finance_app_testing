66be4efb236d7b2a633d578c5aeb4e5566837483
Kir3103 <strunets96@gmail.com>
2023-07-05 23:35:49 +0300

    add parametrization to testFillValues

--- test/specs/testFillValues.js
+++ test/specs/testFillValues.js
@@ -4,15 +4,17 @@ import generateText from '../../utils/generateText.js';
 import generateNumb from '../../utils/generateNumb.js';
 
 describe('Test Fill value on homepage', function (){
+    
+    ['positive', 'negative'].forEach(function (testOption) {
 
-    it('Open homepage, fill value, click button "Save" and check value on homepage', async function(){
+    it(`Open homepage, fill ${testOption} value, click button "Save" and check value on homepage`, async function(done){
 
         await HomePage.isPageOpen();
-        const valueExpense = generateText('positive');
+        const valueExpense = generateText(testOption);
         await HomePage.fillExpenseName(valueExpense);
-        const valueNumb = generateNumb('positive');
+        const valueNumb = generateNumb(testOption);
         await HomePage.fillExpenseAmount(valueNumb);
-        const valueLabel = generateText('positive');
+        const valueLabel = generateText(testOption);
         await HomePage.fillExpenseLabel(valueLabel);
         const arrayWithValuesForFilling = [valueExpense, valueNumb, valueLabel];
         const resultAfterFilling = await HomePage.getTextFromField();
@@ -24,5 +26,6 @@ describe('Test Fill value on homepage', function (){
         const arrayWithValuesFromCompleted = [valueExpense.toUpperCase(), valueNumb, valueLabel];
         const arrayWithValuesAfterSave = await HomePage.getTextFromFieldAfterSave();
         assert.deepEqual(arrayWithValuesAfterSave, arrayWithValuesFromCompleted, 'Values on homepage are different from input values');
+        });
     });
 });
